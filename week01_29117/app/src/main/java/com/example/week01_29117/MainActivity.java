c4c35daab31809c13f12567c119893ffdffaad3f
BrayenBYN <55057553+BrayenBYN@users.noreply.github.com>
2021-05-24 21:36:31 +0700

    Add files via upload

---
+++ week01_29117/app/src/main/java/com/example/week01_29117/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.week01_29117;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
